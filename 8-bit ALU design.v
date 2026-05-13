a6370166f8dc2f352e65647d365b75a01a295758
Ganesh-Tata <ganeshtata115@gmail.com>
2026-05-13 08:12:17 +0530

    Implement 8-bit ALU with basic operations

---
+++ 8-bit ALU design.v
@@ -0,0 +1,26 @@
+module alu_8bit (
+    input  [7:0] A,
+    input  [7:0] B,
+    input  [2:0] opcode,
+    output reg [7:0] result
+);
+
+always @(*) begin
+    case(opcode)
+
+        3'b000: result = A + B; // ADD
+
+        3'b001: result = A - B; // SUB
+
+        3'b010: result = A & B; // AND
+
+        3'b011: result = A | B; // OR
+
+        3'b100: result = A ^ B; // XOR
+
+        default: result = 8'b00000000;
+
+    endcase
+end
+
+endmodule
